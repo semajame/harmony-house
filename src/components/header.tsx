053d79b05a3f0ad20ddd7ccde23ca50b58528892
semajame <jamesanquillano@gmail.com>
2025-05-22 16:16:32 +0800

    update for deployment

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -23,7 +23,7 @@ const Header = () => {
     <header className='bg-[#944EA8] px-5 py-3 text-white'>
       <div className='flex justify-between items-center'>
         {/* Logo + Title */}
-        <a href='/' className='flex items-center gap-2'>
+        <Link href='/' className='flex items-center gap-2'>
           <Image
             src='/images/harmony-house-logo.png'
             alt='Logo Image'
@@ -31,24 +31,24 @@ const Header = () => {
             height={50}
           />
           <span className='text-lg font-semibold'>Harmony House</span>
-        </a>
+        </Link>
 
         {/* Desktop Nav */}
         <nav className='hidden md:flex items-center gap-10'>
           <ul className='flex gap-4 items-center'>
             <li>
-              <a href='' className='hover:underline'>
+              <Link href='/' className='hover:underline px-2'>
                 Home
-              </a>
+              </Link>
             </li>
             <li>
-              <a href='' className='hover:underline'>
+              <Link href='' className='hover:underline px-2'>
                 Contact Us
-              </a>
+              </Link>
             </li>
             <li>
               <DropdownMenu>
-                <DropdownMenuTrigger className='cursor-pointer flex items-center gap-1'>
+                <DropdownMenuTrigger className='cursor-pointer flex items-center gap-1 px-2'>
                   Menu <ChevronDown />
                 </DropdownMenuTrigger>
                 <DropdownMenuContent>
@@ -64,14 +64,14 @@ const Header = () => {
         </nav>
 
         {/* Desktop Buttons */}
-        <div className='hidden md:flex gap-2'>
+        {/* <div className='hidden md:flex gap-2'>
           <Link href='/login'>
             <Button>Login</Button>
           </Link>
           <Link href='/signup'>
             <Button>Register</Button>
           </Link>
-        </div>
+        </div> */}
 
         {/* Mobile Hamburger */}
         <div className='md:hidden'>
@@ -84,12 +84,12 @@ const Header = () => {
       {/* Mobile Nav */}
       {mobileMenuOpen && (
         <div className='md:hidden mt-4 space-y-4 text-center'>
-          <a href='' className='block'>
+          <Link href='/' className='block'>
             Home
-          </a>
-          <a href='' className='block'>
+          </Link>
+          <Link href='/' className='block'>
             Contact Us
-          </a>
+          </Link>
           <div>
             <p className='font-semibold'>Menu</p>
             <ul className='space-y-1'>
@@ -98,14 +98,14 @@ const Header = () => {
               <li>Drinks</li>
             </ul>
           </div>
-          <div className='flex justify-center gap-2 mt-4'>
+          {/* <div className='flex justify-center gap-2 mt-4'>
             <Link href='/login'>
               <Button className='w-24'>Login</Button>
             </Link>
             <Link href='/signup'>
               <Button className='w-24'>Register</Button>
             </Link>
-          </div>
+          </div> */}
         </div>
       )}
     </header>
